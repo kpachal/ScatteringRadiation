d29534f2cb21040e9c56006cd0c49ceeaa676b17
kpachal <katherine.pachal@cern.ch>
2021-10-14 16:10:50 -0700

    Basic skeleton

---
+++ src/run_beam.cxx
@@ -0,0 +1,12 @@
+// Goal: hit electron beam on a target and model all outgoing particles
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "This is a script"
+            << std::endl;
+  return 0;
+}
