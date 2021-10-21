29c1c2770030044fc61857da006f3b23fa3a488f
kpachal <katherine.pachal@cern.ch>
2021-10-21 09:01:07 -0700

    first UI setup

--- src/run_beam.cxx
+++ src/run_beam.cxx
@@ -11,7 +11,10 @@
 // GEANT includes
 #include <FTFP_BERT.hh>
 #include "G4RunManager.hh"
-
+#include "G4UImanager.hh"
+#include "G4VisManager.hh"
+#include "G4UIExecutive.hh"
+#include "G4VisExecutive.hh"
 
 int main(int argc, char* argv[])
 {
@@ -29,8 +32,16 @@ int main(int argc, char* argv[])
   runManager->SetUserAction(new Beam());
 
   // Initialize G4 kernel
-  
 	runManager->Initialize();
+
+  // Setting up the user interface to be nice.
+  G4UIExecutive * ui = new G4UIExecutive(argc, argv);
+  G4VisManager * visManager = new G4VisExecutive();
+  visManager->Initialize();
+  G4UImanager * uiManager = G4UImanager::GetUIpointer();
+  
+  ui->SessionStart();
+  // End of user interface setup  
 
   //Cause the run manager to generate a single event using the
 	//primary generator action registered above.
